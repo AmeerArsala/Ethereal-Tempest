3ac7c3637246fcbcdb9def7385515a12095ec1f3
AmeerArsala <ameer.arsala03@gmail.com>
2020-08-15 15:28:40 -0700

    Updated pathfinding algorithm to actually work

--- src/maps/layout/Path.java
+++ src/maps/layout/Path.java
@@ -5,11 +5,7 @@
  */
 package maps.layout;
 
-import com.jme3.math.FastMath;
-import general.GeneralUtils;
-import general.NodeTree;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -24,7 +20,6 @@ public class Path {
     
     private List<Tile> path = new ArrayList<>();
     private List<Coords> pathSequence = new ArrayList<>();
-    private NodeTree<Coords> blacklist = new NodeTree<>();
     
     public Path(Map map, int startPosX, int startPosY, int destX, int destY, int layer) {
         this.map = map;
@@ -38,46 +33,30 @@ public class Path {
 
         boolean success;
         success = generate(false);
-        System.out.println("success1: " + success);
         
         if (!success) {
-            
-            blacklist.put(GeneralUtils.cloneList(pathSequence));
-            path = new ArrayList<>();
             pathSequence = new ArrayList<>();
             boolean successflip;
             successflip = generate(true);
-            System.out.println("succes2: " + successflip);
-            
-            /*if (!successflip) {
-                blacklist.put(GeneralUtils.cloneList(pathSequence));
-                generateUnbiased();
-            }*/
         }
     }
     
     public List<Tile> getPath() {
         return path;
     }
-    
-    private void generateUnbiased() { //will take into account the blacklist + all possibilities to reach goal
-        pathSequence = new ArrayList<>();
-        
-        while (pathSequence.size() < length) {
-            findUnbiased();
-        }
-        
-        if (pathSequence.get(pathSequence.size() - 1).getX() != destX || pathSequence.get(pathSequence.size() - 1).getY() != destY) {
-            System.out.println("no");
-            blacklist.put(GeneralUtils.cloneList(pathSequence));
-            generateUnbiased();
-            return;
+
+    private boolean generate(boolean flip) { //first option; tries to find path based on its current info
+        boolean success;
+        success = pathfind(flip);
+        while (!pathSequence.get(pathSequence.size() - 1).equals(new Coords(destX, destY))/*.size() < length*/) {
+            success = pathfind(flip);
         }
         
         buildPath();
+        return success;
     }
     
-    private void findUnbiased() {
+    private boolean pathfind(boolean flipPriority) {
         int prevX, prevY;
         if (pathSequence.size() > 0) {
             prevX = pathSequence.get(pathSequence.size() - 1).getX();
@@ -87,73 +66,9 @@ public class Path {
             prevY = startPosY;
         }
         
-        //"possible" options
-        ArrayList<Coords> possibleOptions = new ArrayList<>();
-        possibleOptions.add(new Coords(prevX + 1, prevY)); // right
-        possibleOptions.add(new Coords(prevX, prevY + 1)); // up
-        possibleOptions.add(new Coords(prevX - 1, prevY)); // left
-        possibleOptions.add(new Coords(prevX, prevY - 1)); // down
-        
-        List<Coords> toRemove = new ArrayList<>();
-        
-        //remove options that aren't plausible
-        possibleOptions.stream().filter((option) -> (option.getX() < 0 || option.getY() < 0 || option.getX() >= map.getTilesX() || option.getY() >= map.getTilesY())).forEachOrdered((option) -> {
-            toRemove.add(option);
-        });
-        
-        //remove options that consist of a previous tile
-        if (pathSequence.size() > 0) {
-            pathSequence.forEach((already) -> {
-                possibleOptions.stream().filter((option) -> (already.equals(option))).forEachOrdered((option) -> {
-                    if (!toRemove.contains(option)) {
-                        toRemove.add(option);
-                    }
-                });
-            });
-        }
-        
-        //remove blacklisted options
-        possibleOptions.stream().filter((option) -> (!toRemove.contains(option) && blacklist.getAllChildren(GeneralUtils.addItem(pathSequence, option)).size() > 0 && ((float)FastMath.log((blacklist.getAllChildren(GeneralUtils.addItem(pathSequence, option)).size()), 3)) == ((int)FastMath.log((blacklist.getAllChildren(GeneralUtils.addItem(pathSequence, option)).size()), 3)))).forEachOrdered((option) -> {
-            toRemove.add(option);
-        });
-        
-        toRemove.forEach((die) -> {
-            possibleOptions.remove(die);
-        });
-        
-        //pathSequence.add(possibleOptions.get(FastMath.rand.nextInt(possibleOptions.size())));
-        for (int i = 0; i < possibleOptions.size(); i++) {
-            pathSequence.add(possibleOptions.get(i));
-            if (blacklist.getValueByOrigins(pathSequence) != null) {
-                pathSequence.remove(possibleOptions.get(i));
-            }
-            i = possibleOptions.size();
-        }
-    }
-
-    private boolean generate(boolean flip) { //first option; tries to find path based on its current info
-        boolean success = false;
-        while (path.size() < length) {
-            success = pathfind(flip);
-        }
-        
-        return success;
-    }
-    
-    private boolean pathfind(boolean flipPriority) {
-        int prevX, prevY;
-        if (path.size() > 0) {
-            prevX = path.get(path.size() - 1).getPosX();
-            prevY = path.get(path.size() - 1).getPosY();
-        } else {
-            prevX = startPosX;
-            prevY = startPosY;
-        }
-        
         Coords nextCoords = getNextPathTile(prevX, prevY, flipPriority);
         
         if (nextCoords != null) {
-            path.add(map.fullmap[layer][nextCoords.getX()][nextCoords.getY()]);
             pathSequence.add(nextCoords);
             return true;
         }
@@ -166,37 +81,49 @@ public class Path {
         int yDiff = destY - prevY > 0 ? 1 : (destY - prevY < 0 ? -1 : 0);
         
         Coords tileCoords = new Coords(prevX, prevY);
-        boolean obstacle = false;
         
         if (prevX != destX && !flipPriority) {
-            if (!map.fullmap[layer][prevX + xDiff][prevY].isOccupied) {
-                tileCoords.setX(prevX + xDiff);
-                return tileCoords;
-            } else {
-                obstacle = true;
+            tileCoords.setX(prevX + xDiff);
+            
+            if (sequenceHasCoords(tileCoords) || map.fullmap[layer][tileCoords.getX()][tileCoords.getY()].isOccupied) { //reverts the change if it treads previous ground
+                tileCoords.setX(prevX);
+                tileCoords.setY(prevY + (yDiff != 0 ? yDiff : 1)); //1 is default newYdiff
+                
+                if (sequenceHasCoords(tileCoords) || map.fullmap[layer][tileCoords.getX()][tileCoords.getY()].isOccupied) { //inverts the change if it treads previous ground
+                    tileCoords.setY(prevY - (yDiff != 0 ? yDiff : 1)); //subtract 1
+                }
             }
-        } 
-        if (prevY != destY) {
-            if (!map.fullmap[layer][prevX][prevY + yDiff].isOccupied) {
-                tileCoords.setY(prevY + yDiff);
-                return tileCoords;
-            } else {
-                obstacle = true;
-            }
+            return tileCoords;
         }
-        
-        if (obstacle) {
-        
+        if (prevY != destY) {
+            tileCoords.setY(prevY + yDiff);
+            
+            if (sequenceHasCoords(tileCoords) || map.fullmap[layer][tileCoords.getX()][tileCoords.getY()].isOccupied) { //reverts the change if it treads previous ground
+                tileCoords.setX(prevX + (xDiff != 0 ? xDiff : -1)); //-1 is default newXdiff
+                tileCoords.setY(prevY);
+                
+                if (sequenceHasCoords(tileCoords) || map.fullmap[layer][tileCoords.getX()][tileCoords.getY()].isOccupied) { //inverts the change if it treads previous ground
+                    tileCoords.setX(prevX - (xDiff != 0 ? xDiff : -1)); //subtract -1
+                }
+            }
+            return tileCoords;
         }
         
         return null;
+    }
+    
+    private boolean sequenceHasCoords(Coords cds) {
+        return pathSequence.stream().anyMatch((point) -> (point.equals(cds)));
     }
     
     private void buildPath() {
         path = new ArrayList<>();
         pathSequence.forEach((coord) -> {
             path.add(map.fullmap[layer][coord.getX()][coord.getY()]);
+            System.out.print(coord.toString());
         });
+        System.out.println();
+        
     }
     
     private class Coords {
@@ -225,6 +152,11 @@ public class Path {
         
         public boolean equals(Coords other) {
             return xcoord == other.xcoord && ycoord == other.ycoord;
+        }
+        
+        @Override
+        public String toString() {
+            return "(" + xcoord + ", " + ycoord + ") ";
         }
     }
     
